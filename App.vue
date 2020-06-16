2503164b8d45187ff99f0689c2fc91a662f57c16
CarlaCostea <56835388+CarlaCostea@users.noreply.github.com>
2020-06-17 00:25:51 +0300

    Initial commit

---
+++ App.vue
@@ -0,0 +1,14 @@
+<template>
+    <web-view :source="{uri:'https://invoicemanager.ro/auth.php?app=true'}" />
+</template>
+
+<script>
+import { WebView } from "react-native-webview";
+
+export default {
+  name: "myComponent",
+  components: {
+    "web-view": WebView
+  }
+};
+</script>
